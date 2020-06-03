49d0b06f4868d49406b3ea659ff46e8f4f6470dc
majovainilla <maria_reyes@me.com>
2020-06-03 12:46:24 -0500

    Add routes to App component

---
+++ src/components/TutorList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function TutorList() {
+  return (
+    <div>
+      <h1>Tutor's List</h1>
+    </div>
+  );
+}
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Home from './Home';
+import Appointments from '../components/Appointments';
+import Tutor from '../components/Tutor';
+import User from '../components/User';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/appointments" component={Appointments} />
+          <Route exact path="/tutors/:id" component={Tutor} />
+          <Route exact path="/users/:id" component={User} />
+        </Switch>
+      </Router>
     </div>
   );
 }
