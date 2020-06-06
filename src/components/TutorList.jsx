e49b30b801671fb87fd0711f141758834296d3e3
majovainilla <maria_reyes@me.com>
2020-06-05 23:44:22 -0500

    Add tutor component content and styling

--- src/components/TutorList.jsx
+++ src/components/TutorList.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import tutorClasses from '../styles/tutorList.module.scss';
+import tutors from '../helpers';
+import twitter from '../img/twitter.png';
+import fb from '../img/fb.png';
 
 export default function TutorList() {
   return (
-    <div>
-      <h1>Tutor's List</h1>
+    <div className={tutorClasses.mainDiv}>
+      <div className={tutorClasses.title}>
+        <h1 className={tutorClasses.mainTitle}>Best Tutors</h1>
+        <h2 className={tutorClasses.subTitle}>Choose your teacher</h2>
+        <div className={tutorClasses.divisor}>
+          <p>************</p>
+        </div>
+      </div>
+      <div className={tutorClasses.separator} />
+      {tutors.map(tutor => (
+        <div className={tutorClasses.tutorDiv} key={tutor.id}>
+          <img className={tutorClasses.tutorPic} src={tutor.img} alt="Tutor profile" />
+          <div className={tutorClasses.tutorInfo}>
+            <p className={tutorClasses.tutorName}>{tutor.name}</p>
+            <div className={tutorClasses.divisor}>
+              <p>************</p>
+            </div>
+            <p className={tutorClasses.about}>{tutor.about}</p>
+          </div>
+
+          <div className={tutorClasses.socialDiv}>
+            <img className={tutorClasses.socialIcon} src={twitter} alt="twitter logo" />
+            <img className={tutorClasses.socialIcon} src={fb} alt="Facebook logo" />
+          </div>
+        </div>
+      ))}
+      <div className={tutorClasses.nextButton}>
+        >
+      </div>
     </div>
   );
 }
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar';
 import Home from './Home';
 import Appointment from '../components/Appointment';
 import Tutor from '../components/Tutor';
+import TutorList from '../components/TutorList';
 import User from '../components/User';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/appointments" component={Appointment} />
+          <Route exact path="/tutors/" component={TutorList} />
           <Route exact path="/tutors/:id" component={Tutor} />
           <Route exact path="/users/:id" component={User} />
         </Switch>
