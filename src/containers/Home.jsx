74ad17c6e0ec7c0f5023527917045e1ddb4ee5b9
majovainilla <maria_reyes@me.com>
2020-06-11 17:06:51 -0500

    Edit navbar route links

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,27 +3,31 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import homeClasses from '../styles/home.module.scss';
+import { TutorList } from '../components/TutorList';
 
-export function Home(props) {
-  const { loggedIn } = props;
+export default function Home(props) {
+  // const { loggedIn } = props;
+  const token = localStorage.getItem('token');
 
-  if (loggedIn === false) { return (<Redirect to="/auth/login" />); }
+  // if (loggedIn === false) { return (<Redirect to="/auth/login" />); }
+  if (!token) { return (<Redirect to="/auth/login" />); }
 
   return (
     <div className={homeClasses.home}>
       <div className={homeClasses.bgImage} />
+      <TutorList />
     </div>
   );
 }
 
-const mapStateToProps = state => ({
-  loggedIn: state.auth.loggedIn,
-});
+// const mapStateToProps = state => ({
+//   loggedIn: state.auth.loggedIn,
+// });
 
 
-Home.propTypes = {
-  loggedIn: PropTypes.bool.isRequired,
-};
+// Home.propTypes = {
+//   loggedIn: PropTypes.bool.isRequired,
+// };
 
 
-export default connect(mapStateToProps)(Home);
+// export default connect(mapStateToProps)(Home);
