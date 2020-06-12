9b12031712f5c5a42317fdd8909f4a10b96e121b
majovainilla <maria_reyes@me.com>
2020-06-12 12:00:57 -0500

    Edit tutor to handle redirect when not logged in

--- src/components/Tutor.jsx
+++ src/components/Tutor.jsx
@@ -20,7 +20,7 @@ export default class Tutor extends Component {
   componentDidMount() {
     const { id } = this.state;
     if (this.token) {
-    axios.get(`https://appointments-api-majovanilla.herokuapp.com/tutors/${this.state.id}`, {
+      axios.get(`https://appointments-api-majovanilla.herokuapp.com/tutors/${id}`, {
         headers: { Authorization: `Bearer ${this.token}` },
       }).then(response => {
         this.setState({ tutor: response.data });
