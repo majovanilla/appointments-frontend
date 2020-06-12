7576228774931ee65bfd7779baf1bf4269b4473a
majovainilla <maria_reyes@me.com>
2020-06-12 10:47:28 -0500

    Configuration working on development

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,27 +3,29 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import homeClasses from '../styles/home.module.scss';
+// import { TutorList } from '../components/TutorList';
+import Login from './auth/Login';
 
-export function Home(props) {
-  const { loggedIn } = props;
-
-  if (loggedIn === false) { return (<Redirect to="/auth/login" />); }
+export default function Home(props) {
+  // const { loggedIn } = props;
+  const token = localStorage.getItem('token');
 
   return (
-    <div className={homeClasses.home}>
-      <div className={homeClasses.bgImage} />
+    <div className={homeClasses.bgImage}>
+      <Login />
     </div>
+    // <Redirect to="/tutors" />
   );
 }
 
-const mapStateToProps = state => ({
-  loggedIn: state.auth.loggedIn,
-});
+// const mapStateToProps = state => ({
+//   loggedIn: state.auth.loggedIn,
+// });
 
 
-Home.propTypes = {
-  loggedIn: PropTypes.bool.isRequired,
-};
+// Home.propTypes = {
+//   loggedIn: PropTypes.bool.isRequired,
+// };
 
 
-export default connect(mapStateToProps)(Home);
+// export default connect(mapStateToProps)(Home);
